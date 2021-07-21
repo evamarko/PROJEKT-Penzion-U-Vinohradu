20dbf092b4af56f39abe0ecf3bc06f873da15e0f
eva.valentova <valentova.eva92@gmail.com>
2021-07-21 14:30:39 +0200

    Přidán favicon

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
   // Zkopírovat css/ to _site/css/
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("favicon");
 
   return {
     // možné formáty šablon
