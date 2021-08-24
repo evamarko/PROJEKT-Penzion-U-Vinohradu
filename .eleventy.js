4a6c23a7b7eb74619d77c93611eee411cb683182
eva.valentova <valentova.eva92@gmail.com>
2021-08-24 18:24:45 +0200

    Robots

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("favicon");
+  eleventyConfig.addPassthroughCopy("robots");
 
   return {
     // možné formáty šablon
